450396480d451e33e63da225bb5c40dcccfda74b
Steve Willoughby <steve@madscience.zone>
2022-05-01 16:42:45 -0700

    initial "hello, world" cut

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,30 @@
 Game Master's Assistant / Mapper Client
 Release Notes
-Version 3.41.0 * Sunday, 26th September, 2021
+Version UNNAMED * Sunday, 26th September, 2021
 
 Represents:
- * Supported GMA Mapper Version: 3.41.0      <!-- @@##@@ -->
- * Supported GMA Mapper File Format: 17	      <!-- @@##@@ -->
- * Supported GMA Mapper Protocol: 332         <!-- @@##@@ -->
+ * Supported GMA Mapper Version: 4.x.x      <!-- @@##@@ -->
+ * Supported GMA Mapper File Format: 20	      <!-- @@##@@ -->
+ * Supported GMA Mapper Protocol: 400         <!-- @@##@@ -->
+
+# CURRENT
+This version represents a complete rewrite of the mapper client.
+A rewrite has been needed for some time to clean up the code and
+overall code structure. I decided this time to do the rewrite by
+porting the client to Go instead of keeping it in Tcl/Tk or porting
+it to Python. This was based on our experience to date with the mapper
+and other tools, and the fact that the mapper, more than any other tool,
+should be as simple to install and use as possible, in addition to being
+fast and robust. By compiling ready-to-run clients in Go, we can deliver
+a binary execuatble (say, `mapper.exe` on Windows) which just works without
+needing to install and tweak numerous external dependencies.
+
+## Enhancements
+* This version moves the mapper to protocol 400, which drops the old-style
+protocol using Tcl lists for serialization in favor of JSON.
+* This version also updates the file formats for `.map` files to format 20,
+which drops the older multi-line key-value TCL-based file format for a more
+structured one based on JSON.
 
 # 3.41.0
 ## Enhancements
---
+++ cmd/mapper/main.go
@@ -0,0 +1,95 @@
+/*
+########################################################################################
+#  _______  _______  _______             ______         ___     __       _______       #
+# (  ____ \(       )(  ___  ) Game      / ___  \       /   )   /  \     (  __   )      #
+# | (    \/| () () || (   ) | Master's  \/   \  \     / /) |   \/) )    | (  )  |      #
+# | |      | || || || (___) | Assistant    ___) /    / (_) (_    | |    | | /   |      #
+# | | ____ | |(_)| ||  ___  |             (___ (    (____   _)   | |    | (/ /) |      #
+# | | \_  )| |   | || (   ) |                 ) \        ) (     | |    |   / | |      #
+# | (___) || )   ( || )   ( | Mapper    /\___/  / _      | |   __) (_ _ |  (__) |      #
+# (_______)|/     \||/     \| Client    \______/ (_)     (_)   \____/(_)(_______)      #
+#                                                                                      #
+########################################################################################
+*/
+
+//
+// GMA Mapper Client with background I/O processing.
+//
+package main
+
+import "github.com/visualfc/atk/tk"
+
+//
+// Auto-configure values
+//
+const (
+	GMAMapperVersion    = "3.41.0" // @@##@@
+	GMAMapperFileFormat = 20       // @@##@@
+	GMAMapperProtocol   = 400      // @@##@@
+	GMAVersionNumber    = "4.3.11" // @@##@@
+)
+
+type Window struct {
+	*tk.Window
+}
+
+func NewWindow() *Window {
+	mw := &Window{tk.RootWindow()}
+	lbl := tk.NewLabel(mw, "Hello ATK")
+	btn := tk.NewButton(mw, "Quit")
+	btn.OnCommand(func() {
+		tk.Quit()
+	})
+	tk.NewVPackLayout(mw).AddWidgets(lbl, tk.NewLayoutSpacer(mw, 0, true), btn)
+	mw.ResizeN(300, 200)
+	return mw
+}
+
+func main() {
+	tk.MainLoop(func() {
+		mw := NewWindow()
+		mw.SetTitle("ATK Sample")
+		//		mw.Center(nil)
+		mw.ShowNormal()
+	})
+}
+
+// @[00]@| GMA 4.3.11
+// @[01]@|
+// @[10]@| Copyright © 1992–2021 by Steven L. Willoughby
+// @[11]@| steve@madscience.zone (previously AKA Software Alchemy),
+// @[12]@| Aloha, Oregon, USA. All Rights Reserved.
+// @[13]@| Distributed under the terms and conditions of the BSD-3-Clause
+// @[14]@| License as described in the accompanying LICENSE file distributed
+// @[15]@| with GMA.
+// @[16]@|
+// @[20]@| Redistribution and use in source and binary forms, with or without
+// @[21]@| modification, are permitted provided that the following conditions
+// @[22]@| are met:
+// @[23]@| 1. Redistributions of source code must retain the above copyright
+// @[24]@|    notice, this list of conditions and the following disclaimer.
+// @[25]@| 2. Redistributions in binary form must reproduce the above copy-
+// @[26]@|    right notice, this list of conditions and the following dis-
+// @[27]@|    claimer in the documentation and/or other materials provided
+// @[28]@|    with the distribution.
+// @[29]@| 3. Neither the name of the copyright holder nor the names of its
+// @[30]@|    contributors may be used to endorse or promote products derived
+// @[31]@|    from this software without specific prior written permission.
+// @[32]@|
+// @[33]@| THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+// @[34]@| CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES,
+// @[35]@| INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// @[36]@| MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// @[37]@| DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+// @[38]@| BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+// @[39]@| OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// @[40]@| PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// @[41]@| PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// @[42]@| THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+// @[43]@| TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// @[44]@| THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+// @[45]@| SUCH DAMAGE.
+// @[46]@|
+// @[50]@| This software is not intended for any use or application in which
+// @[51]@| the safety of lives or property would be at risk due to failure or
+// @[52]@| defect of the software.
