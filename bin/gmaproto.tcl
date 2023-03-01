89b8763095a5fbc7f8091e528a61b6436ad7abef
Steve Willoughby <steve@madscience.zone>
2023-02-28 19:02:02 -0800

    added support for protocol 401 AV command

--- bin/gmaproto.tcl
+++ bin/gmaproto.tcl
@@ -10,45 +10,6 @@
 #                                                                                      #
 ########################################################################################
 #
-# @[00]@| GMA 5.0.0
-# @[01]@|
-# @[10]@| Copyright © 1992–2023 by Steven L. Willoughby (AKA MadScienceZone)
-# @[11]@| steve@madscience.zone (previously AKA Software Alchemy),
-# @[12]@| Aloha, Oregon, USA. All Rights Reserved.
-# @[13]@| Distributed under the terms and conditions of the BSD-3-Clause
-# @[14]@| License as described in the accompanying LICENSE file distributed
-# @[15]@| with GMA.
-# @[16]@|
-# @[20]@| Redistribution and use in source and binary forms, with or without
-# @[21]@| modification, are permitted provided that the following conditions
-# @[22]@| are met:
-# @[23]@| 1. Redistributions of source code must retain the above copyright
-# @[24]@|    notice, this list of conditions and the following disclaimer.
-# @[25]@| 2. Redistributions in binary form must reproduce the above copy-
-# @[26]@|    right notice, this list of conditions and the following dis-
-# @[27]@|    claimer in the documentation and/or other materials provided
-# @[28]@|    with the distribution.
-# @[29]@| 3. Neither the name of the copyright holder nor the names of its
-# @[30]@|    contributors may be used to endorse or promote products derived
-# @[31]@|    from this software without specific prior written permission.
-# @[32]@|
-# @[33]@| THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
-# @[34]@| CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES,
-# @[35]@| INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-# @[36]@| MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# @[37]@| DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
-# @[38]@| BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-# @[39]@| OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# @[40]@| PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-# @[41]@| PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# @[42]@| THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-# @[43]@| TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
-# @[44]@| THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-# @[45]@| SUCH DAMAGE.
-# @[46]@|
-# @[50]@| This software is not intended for any use or application in which
-# @[51]@| the safety of lives or property would be at risk due to failure or
-# @[52]@| defect of the software.
 #
 # Mapper JSON protocol handling functions.
 # Steve Willoughby <steve@madscience.zone>
@@ -88,7 +49,7 @@
 # 	::report_progress message
 # 	::say message
 
-package provide gmaproto 1.0
+package provide gmaproto 1.1
 package require Tcl 8.5
 package require json 1.3.4
 package require json::write 1.0.3
@@ -96,9 +57,9 @@ package require base64 2.4.2
 package require uuid 1.0.1
 
 namespace eval ::gmaproto {
-	variable protocol 400
+	variable protocol 401
 	variable min_protocol 333
-	variable max_protocol 400
+	variable max_protocol 401
 	variable debug_f {}
 	variable legacy false
 	variable host {}
@@ -164,7 +125,7 @@ namespace eval ::gmaproto {
 		AI?	{Name s Sizes {a {Zoom f}}}
 		ALLOW   {Features l}
 		AUTH    {Client s Response b User s}
-		AV      {XView f YView f}
+		AV      {Grid s XView f YView f}
 		CC      {RequestedBy s DoSilently ? Target i MessageID i}
 		CLR     {ObjID s}
 		CLR@    {File s IsLocalFile ?}
@@ -1266,8 +1227,8 @@ proc ::gmaproto::_construct {input types} {
 	return $input
 }
 
-proc ::gmaproto::adjust_view {x y} {
-	::gmaproto::_protocol_send AV XView $x YView $y
+proc ::gmaproto::adjust_view {x y grid_label} {
+	::gmaproto::_protocol_send AV Grid $grid_label XView $x YView $y
 }
 proc ::gmaproto::chat_message {message sender recipients to_all to_gm} {
 	::gmaproto::_protocol_send TO Recipients $recipients ToAll $to_all ToGM $to_gm Text $message
@@ -2142,3 +2103,42 @@ proc ::gmaproto::GMATypeToProtocolCommand {gt} {
 	}
 	return $gt
 }
+# @[00]@| GMA 5.0.0
+# @[01]@|
+# @[10]@| Copyright © 1992–2023 by Steven L. Willoughby (AKA MadScienceZone)
+# @[11]@| steve@madscience.zone (previously AKA Software Alchemy),
+# @[12]@| Aloha, Oregon, USA. All Rights Reserved.
+# @[13]@| Distributed under the terms and conditions of the BSD-3-Clause
+# @[14]@| License as described in the accompanying LICENSE file distributed
+# @[15]@| with GMA.
+# @[16]@|
+# @[20]@| Redistribution and use in source and binary forms, with or without
+# @[21]@| modification, are permitted provided that the following conditions
+# @[22]@| are met:
+# @[23]@| 1. Redistributions of source code must retain the above copyright
+# @[24]@|    notice, this list of conditions and the following disclaimer.
+# @[25]@| 2. Redistributions in binary form must reproduce the above copy-
+# @[26]@|    right notice, this list of conditions and the following dis-
+# @[27]@|    claimer in the documentation and/or other materials provided
+# @[28]@|    with the distribution.
+# @[29]@| 3. Neither the name of the copyright holder nor the names of its
+# @[30]@|    contributors may be used to endorse or promote products derived
+# @[31]@|    from this software without specific prior written permission.
+# @[32]@|
+# @[33]@| THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+# @[34]@| CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# @[35]@| INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# @[36]@| MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# @[37]@| DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+# @[38]@| BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+# @[39]@| OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# @[40]@| PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# @[41]@| PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# @[42]@| THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+# @[43]@| TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+# @[44]@| THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# @[45]@| SUCH DAMAGE.
+# @[46]@|
+# @[50]@| This software is not intended for any use or application in which
+# @[51]@| the safety of lives or property would be at risk due to failure or
+# @[52]@| defect of the software.
