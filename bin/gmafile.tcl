01f9d6f32790f94e04cadb0ad1e6a16f74a3ae2a
Steve Willoughby <steve@madscience.zone>
2023-05-29 20:02:07 -0700

    polymorph fix

--- bin/gmafile.tcl
+++ bin/gmafile.tcl
@@ -10,7 +10,7 @@
 #                                                                                      #
 ########################################################################################
 #
-# @[00]@| GMA-Mapper 4.10-alpha
+# @[00]@| GMA-Mapper 4.10-alpha.2
 # @[01]@|
 # @[10]@| Copyright © 1992–2023 by Steven L. Willoughby (AKA MadScienceZone)
 # @[11]@| steve@madscience.zone (previously AKA Software Alchemy),
