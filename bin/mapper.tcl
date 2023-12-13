df990d5391083945c7ba1de15e8c6e405e86414d
Steve Willoughby <steve@madscience.zone>
2023-12-13 12:29:33 -0800

    release 4.19

--- bin/mapper.tcl
+++ bin/mapper.tcl
@@ -17,10 +17,10 @@
 # GMA Mapper Client with background I/O processing.
 #
 # Auto-configure values
-set GMAMapperVersion {4.18.2}     ;# @@##@@
+set GMAMapperVersion {4.19}     ;# @@##@@
 set GMAMapperFileFormat {22}        ;# @@##@@
-set GMAMapperProtocol {408}         ;# @@##@@
-set CoreVersionNumber {6.9.1}            ;# @@##@@
+set GMAMapperProtocol {409}         ;# @@##@@
+set CoreVersionNumber {6.10}            ;# @@##@@
 encoding system utf-8
 #---------------------------[CONFIG]-------------------------------------------
 #
@@ -13135,7 +13135,7 @@ proc ConnectToServerByIdx {idx} {
 #   .../<name>@<zoom>/:<frame>:<name>@<zoom>.<ext>
 #   .../<name>.map
 
-# @[00]@| GMA-Mapper 4.18.2
+# @[00]@| GMA-Mapper 4.19
 # @[01]@|
 # @[10]@| Copyright © 1992–2023 by Steven L. Willoughby (AKA MadScienceZone)
 # @[11]@| steve@madscience.zone (previously AKA Software Alchemy),
