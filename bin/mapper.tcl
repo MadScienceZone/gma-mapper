5b2143feb07638ff76ec395ec519687f0f2e3d26
Steve Willoughby <steve@madscience.zone>
2025-11-07 19:10:27 -0800

    4.34 release staging

--- bin/mapper.tcl
+++ bin/mapper.tcl
@@ -1,13 +1,13 @@
 #!/usr/bin/env wish
 ########################################################################################
-#  _______  _______  _______                ___       ______   ______       __         #
-# (  ____ \(       )(  ___  ) Game         /   )     / ___  \ / ___  \     /  \        #
-# | (    \/| () () || (   ) | Master's    / /) |     \/   \  \\/   \  \    \/) )       #
-# | |      | || || || (___) | Assistant  / (_) (_       ___) /   ___) /      | |       #
-# | | ____ | |(_)| ||  ___  |           (____   _)     (___ (   (___ (       | |       #
-# | | \_  )| |   | || (   ) |                ) (           ) \      ) \      | |       #
-# | (___) || )   ( || )   ( | Mapper         | |   _ /\___/  //\___/  / _  __) (_      #
-# (_______)|/     \||/     \| Client         (_)  (_)\______/ \______/ (_) \____/      #
+#  _______  _______  _______                ___       ______      ___                  #
+# (  ____ \(       )(  ___  ) Game         /   )     / ___  \    /   )                 #
+# | (    \/| () () || (   ) | Master's    / /) |     \/   \  \  / /) |                 #
+# | |      | || || || (___) | Assistant  / (_) (_       ___) / / (_) (_                #
+# | | ____ | |(_)| ||  ___  |           (____   _)     (___ ( (____   _)               #
+# | | \_  )| |   | || (   ) |                ) (           ) \     ) (                 #
+# | (___) || )   ( || )   ( | Mapper         | |   _ /\___/  /     | |                 #
+# (_______)|/     \||/     \| Client         (_)  (_)\______/      (_)                 #
 #                                                                                      #
 ########################################################################################
 # TODO move needs to move entire animated stack (seems to do the right thing when mapper is restarted)
@@ -17,10 +17,10 @@
 # GMA Mapper Client with background I/O processing.
 #
 # Auto-configure values
-set GMAMapperVersion {4.33.1}     ;# @@##@@
+set GMAMapperVersion {4.34}     ;# @@##@@
 set GMAMapperFileFormat {23}        ;# @@##@@
 set GMAMapperProtocol {419}         ;# @@##@@
-set CoreVersionNumber {6.34.1}            ;# @@##@@
+set CoreVersionNumber {6.36.1}            ;# @@##@@
 encoding system utf-8
 #---------------------------[CONFIG]-------------------------------------------
 #
@@ -16545,7 +16545,7 @@ proc EncodePresetDetails {p} {
 #
 #
 #
-# @[00]@| GMA-Mapper 4.33.1
+# @[00]@| GMA-Mapper 4.34
 # @[01]@|
 # @[10]@| Overall GMA package Copyright © 1992–2025 by Steven L. Willoughby (AKA MadScienceZone)
 # @[11]@| steve@madscience.zone (previously AKA Software Alchemy),
