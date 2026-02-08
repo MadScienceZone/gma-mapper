b7f24fac3262c964b11c2764147239997a280650
Steve Willoughby <steve@madscience.zone>
2026-02-08 05:14:46 -0800

    release 4.36.7

--- bin/mapper.tcl
+++ bin/mapper.tcl
@@ -1,13 +1,13 @@
 #!/usr/bin/env wish
 ########################################################################################
-#  _______  _______  _______                ___       ______    ______      ______     #
-# (  ____ \(       )(  ___  ) Game         /   )     / ___  \  / ____ \    / ____ \    #
-# | (    \/| () () || (   ) | Master's    / /) |     \/   \  \( (    \/   ( (    \/    #
-# | |      | || || || (___) | Assistant  / (_) (_       ___) /| (____     | (____      #
-# | | ____ | |(_)| ||  ___  |           (____   _)     (___ ( |  ___ \    |  ___ \     #
-# | | \_  )| |   | || (   ) | VTT            ) (           ) \| (   ) )   | (   ) )    #
-# | (___) || )   ( || )   ( | Mapper         | |   _ /\___/  /( (___) ) _ ( (___) )    #
-# (_______)|/     \||/     \| Client         (_)  (_)\______/  \_____/ (_) \_____/     #
+#  _______  _______  _______                ___       ______    ______     ______      #
+# (  ____ \(       )(  ___  ) Game         /   )     / ___  \  / ____ \   / ___  \     #
+# | (    \/| () () || (   ) | Master's    / /) |     \/   \  \( (    \/   \/   )  )    #
+# | |      | || || || (___) | Assistant  / (_) (_       ___) /| (____         /  /     #
+# | | ____ | |(_)| ||  ___  |           (____   _)     (___ ( |  ___ \       /  /      #
+# | | \_  )| |   | || (   ) | VTT            ) (           ) \| (   ) )     /  /       #
+# | (___) || )   ( || )   ( | Mapper         | |   _ /\___/  /( (___) ) _  /  /        #
+# (_______)|/     \||/     \| Client         (_)  (_)\______/  \_____/ (_) \_/         #
 #                                                                                      #
 ########################################################################################
 # TODO move needs to move entire animated stack (seems to do the right thing when mapper is restarted)
@@ -17,10 +17,10 @@
 # GMA Mapper Client with background I/O processing.
 #
 # Auto-configure values
-set GMAMapperVersion {4.36.7-alpha}     ;# @@##@@
+set GMAMapperVersion {4.36.7}     ;# @@##@@
 set GMAMapperFileFormat {23}        ;# @@##@@
 set GMAMapperProtocol {422}         ;# @@##@@
-set CoreVersionNumber {6.41}            ;# @@##@@
+set CoreVersionNumber {6.42}            ;# @@##@@
 encoding system utf-8
 #---------------------------[CONFIG]-------------------------------------------
 #
@@ -10531,6 +10531,10 @@ proc DoCommandHPACK {d} {
 	ihr_accepted [dict get $d RequestID]
 }
 
+proc DoCommandHPREQ {d} {
+	# clients ignore these upon receipt
+}
+
 proc DoCommandFAILED {d} {
 	# Indicate general failure. If we're still showing a timer request dialog, update it based on
 	# IsError, IsDiscretionary, Reason, ReqeustID
@@ -17785,7 +17789,7 @@ proc check_aka_commit {} {
 #
 #  called when rendering somone or advancing the initiative turn or updating target attribute
 #
-# @[00]@| GMA-Mapper 4.36.6
+# @[00]@| GMA-Mapper 4.36.7
 # @[01]@|
 # @[10]@| Overall GMA package Copyright © 1992–2026 by Steven L. Willoughby (AKA MadScienceZone)
 # @[11]@| steve@madscience.zone (previously AKA Software Alchemy),
