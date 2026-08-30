47eb7a4a3ed71b4ba811611e50d3415c0798970e
Steve Willoughby <steve@madscience.zone>
2026-08-30 02:34:01 -0500

    version

--- bin/mapper.tcl
+++ bin/mapper.tcl
@@ -27,7 +27,7 @@
 # GMA Mapper Client with background I/O processing.
 #
 # Auto-configure values
-set GMAMapperVersion {4.40.3}     ;# @@##@@
+set GMAMapperVersion {4.41-alpha}     ;# @@##@@
 set GMAMapperFileFormat {24}        ;# @@##@@
 set GMAMapperProtocol {426}         ;# @@##@@
 set CoreVersionNumber {6.47}            ;# @@##@@
